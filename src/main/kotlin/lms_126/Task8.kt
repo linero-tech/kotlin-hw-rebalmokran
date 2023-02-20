6e155b2394f27b08ed232ae524cb89c06cf36d66
rebalmokran <rebalmokran24@gmail.com>
2023-02-20 22:35:03 +0100

    task 8 101

--- src/main/kotlin/lms_126/Task8.kt
+++ src/main/kotlin/lms_126/Task8.kt
@@ -2,5 +2,15 @@ package lms_126
 
 fun task8(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return /*
+    if x % 3 == 0
+    output foo
+    if x % 5 == 0
+    output bar
+    if x % 3 == 0 and x % 5 == 0
+    output foobar
+    else
+    output
+    qix
+    */
 }
