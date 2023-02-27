ff5057cd9fffb23989d1339e06a44404335f69f4
rebalmokran <rebalmokran24@gmail.com>
2023-02-27 11:18:06 +0100

    task 6 102

--- src/main/kotlin/lms_127/Task6.kt
+++ src/main/kotlin/lms_127/Task6.kt
@@ -2,5 +2,11 @@ package lms_127
 
 fun task6(base1: Double, base2: Double, height: Double): Double {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val result = (base1 + base2) * height /2
+
+    return result
+}
+
+fun main () {
+    println(task6(base1= 10.0, base2= 20.0, height =1.0))
 }
