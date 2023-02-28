5e35fbe6f44504db47fcfbd1666527e7f3d2487c
rebalmokran <rebalmokran24@gmail.com>
2023-02-28 16:30:13 +0100

    update task 8 101

--- src/main/kotlin/lms_126/Task8.kt
+++ src/main/kotlin/lms_126/Task8.kt
@@ -1,16 +1,16 @@
 package lms_126
 
-fun task8(): String {
+import kotlin.test.todo
 
-    return /*
-    if x % 3 == 0
-    output foo
-    if x % 5 == 0
-    output bar
+fun task8(): String {
+    return """
     if x % 3 == 0 and x % 5 == 0
     output foobar
+    else if x % 3 == 0
+    output foo
+    else if x % 5 == 0
+    output bar
     else
-    output
-    qix
-    */
+    output qix
+    """
 }
