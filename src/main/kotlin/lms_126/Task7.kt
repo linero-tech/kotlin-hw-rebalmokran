c73a3e702abea4ae9155a9e0d2aed9636f052f88
rebalmokran <rebalmokran24@gmail.com>
2023-02-28 16:29:44 +0100

    update task 7 101

--- src/main/kotlin/lms_126/Task7.kt
+++ src/main/kotlin/lms_126/Task7.kt
@@ -1,16 +1,11 @@
 package lms_126
 
 fun task7(): String {
-
-    return /*
+    return """
     for number 1-100
     if number is odd
     output number
     else output
     error
-
-
-
-
-*/
+"""
 }
