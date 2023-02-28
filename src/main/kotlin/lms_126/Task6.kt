3db8967168e12d6241e9841010049ba126e833e9
rebalmokran <rebalmokran24@gmail.com>
2023-02-28 16:29:26 +0100

    update task 6 101

--- src/main/kotlin/lms_126/Task6.kt
+++ src/main/kotlin/lms_126/Task6.kt
@@ -2,22 +2,11 @@ package lms_126
 
 fun task6(): String {
 
-    return /*
-    input 1-25 * 5
-    output number 
-    5 * 1 = 5
-    5 * 2 = 10
-    5 * 3 = 15
-    5 * 4 = 20
-    5 * 5 = 25
-    */
-    ....
-/*
-   for number 1 to 100
-   if number is multiples of 5
-   output number
-   else
-   output error
-
-    */
+    return """
+       for number 1 to 100
+       if number is multiples of 5
+       output number
+       else
+       output error
+       """
 }
