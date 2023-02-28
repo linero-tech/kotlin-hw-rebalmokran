17c05fb7e689b6daefafed8268aada0b6f686ee6
rebalmokran <rebalmokran24@gmail.com>
2023-02-28 16:31:26 +0100

    update task 10 101

--- src/main/kotlin/lms_126/Task10.kt
+++ src/main/kotlin/lms_126/Task10.kt
@@ -2,17 +2,16 @@ package lms_126
 
 fun task10(): String {
 
-    return /*
-    input email
-    input password
-    if email is mal-formed:
-    output error
-    else
-    if password is less than 6 characters long:
-    output error
-    else
-    create an account
-    */
-
-
+    return """
+        input email
+        input password
+        if email is mal-formed:
+        output error
+        else
+        if password is less than 6 characters long:
+        output error
+        else
+        create an account
+        */
+        """
 }
