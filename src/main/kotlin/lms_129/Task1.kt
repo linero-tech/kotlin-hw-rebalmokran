d8138bd94efa6fc99e878f0a08fb7a1e98a5b0b5
rebalmokran <rebalmokran24@gmail.com>
2023-03-02 15:19:19 +0100

    task 1 103

--- src/main/kotlin/lms_129/Task1.kt
+++ src/main/kotlin/lms_129/Task1.kt
@@ -1,6 +1,19 @@
 package lms_129
 
-fun task1(a: Int, b: Int): Int {
+fun task1(a: Int, b: Int) {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = 0
+
+    if (a >= b) {
+        result
+    } else
+        for (number in a..b) {
+            result += number
+        }
+    println(result)
+
+}
+
+fun main() {
+    task1(a = 6, b = 5)
 }
