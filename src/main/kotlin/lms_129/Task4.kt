c88ba893854eb923932fa18a9ca0651f83824298
rebalmokran <rebalmokran24@gmail.com>
2023-03-09 17:02:00 +0100

    task 4 103

--- src/main/kotlin/lms_129/Task4.kt
+++ src/main/kotlin/lms_129/Task4.kt
@@ -2,5 +2,15 @@ package lms_129
 
 fun task4(): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = 0
+    for (i in 1..1000) {
+        if (i % 9 == 0) {
+            println(i)
+        }
+    }
+    return result
 }
+fun main () {
+    println(task4())
+}
+
