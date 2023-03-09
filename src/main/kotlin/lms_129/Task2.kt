fa0fb98c9d079d172bf44f5671a004757c7e973c
rebalmokran <rebalmokran24@gmail.com>
2023-03-09 16:15:07 +0100

    task 2 103

--- src/main/kotlin/lms_129/Task2.kt
+++ src/main/kotlin/lms_129/Task2.kt
@@ -1,6 +1,26 @@
 package lms_129
 
-fun task2(number: Int): Boolean {
+import kotlin.math.sqrt
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+fun task2(number: Int): Boolean {
+    var result = true
+
+    if (number < 2) {
+        result = false
+    } else {
+        for (i in 2 until number) {
+            if (number % i == 0) {
+                result = false
+                break
+            }
+        }
+    }
+    return result
 }
+    fun main (){
+    val number = 1
+    val isPrime = task2(number)
+    println("Number $number is prime: $isPrime")
+}
+
+
